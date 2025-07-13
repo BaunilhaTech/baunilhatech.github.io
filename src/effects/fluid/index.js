b9a3db4a4a48aed162de569947ebb19cdb2eea46
Alê AmbevTech <alexandre.alvaro@ambevtech.com.br>
2025-07-13 01:13:41 +0000

    feat: remove footer overlay for improved design; delete unused interactive pointer script

--- src/effects/fluid/index.js
+++ src/effects/fluid/index.js
@@ -13,7 +13,6 @@
  */
 
 export { FluidSimulation as FluidEffect } from "./fluid-simulation.js";
-export { FooterFluidSimulation as FooterFluidEffect } from "./footer-fluid-simulation.js";
 
 // Exporta também as configurações padrão para customização
 export const defaultFluidConfig = {
